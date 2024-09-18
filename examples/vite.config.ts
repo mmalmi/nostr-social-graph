eb0c7750a4811285e480e2d801b43fa1c8a7b6d3
Martti Malmi <sirius@iki.fi>
2024-09-18 08:40:55 +0300

    update vite

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'src',
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: 'src/index.html',
+      input: 'index.html',
     },
     outDir: 'dist',
   },
